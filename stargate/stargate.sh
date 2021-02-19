6ebe5ade20c7dfa0b60af649964903b0bb2e6d3f
Gianmarco Fraccaroli <gianmarco.fraccaroli@xpeppers.com>
2021-02-19 15:24:14 +0100

    [feat]: change PATH to NODE_PATH

--- stargate/stargate.sh
+++ stargate/stargate.sh
@@ -2,25 +2,25 @@
 
 #variables
 NAME="stargate"
-PATH="~/node-${NAME}"
+NODE_PATH="~/node-${NAME}"
 
 # create data folder
-mkdir ${PATH}
+mkdir ${NODE_PATH}
 
 # init node
-gaiad init heliax-${NAME} --home=${PATH}
+gaiad init heliax-${NAME} --home=${NODE_PATH}
 
 # move genesis
-rm "${PATH}/config/genesis.json"
+rm "${NODE_PATH}/config/genesis.json"
 unzip "stargate/genesis.zip" -d "stargate"
-cp "stargate/genesis.json" "${PATH}/config/genesis.json"
+cp "stargate/genesis.json" "${NODE_PATH}/config/genesis.json"
 
 # copy config
-rm "${PATH}/config/config.toml"
-cp stargate/genesis.json "${PATH}/config/toml.json"
+rm "${NODE_PATH}/config/config.toml"
+cp stargate/genesis.json "${NODE_PATH}/config/toml.json"
 
 # generate key
-gaiad --home --home=${PATH} keys add user --keyring-backend="test" --output json > ${PATH}/key_seed.json 2> /dev/null
+gaiad --home --home=${NODE_PATH} keys add user --keyring-backend="test" --output json > ${NODE_PATH}/key_seed.json 2> /dev/null
 
 # start node
-gaiad start --home=${PATH} --log_level=info --x-crisis-skip-assert-invariants --pruning=nothing --grpc.address="0.0.0.0:9090"
+gaiad start --home=${NODE_PATH} --log_level=info --x-crisis-skip-assert-invariants --pruning=nothing --grpc.address="0.0.0.0:9090"
