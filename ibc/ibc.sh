974a56d40093a015ce60f0fef79957ab488140b9
Fraccaroli Gianmarco <gianmarco.fraccaroli@gmail.com>
2021-02-22 11:41:45 +0100

    [fix] bash

--- ibc/ibc.sh
+++ ibc/ibc.sh
@@ -23,7 +23,7 @@ cargo run --bin hermes -- -c ~/.hermes/config.toml light add "localhost:26357" -
 # set the secondary peers for clients on each chain
 echo "Adding secondary peers to light client configuration..."
 cargo run --bin hermes -- -c ~/.hermes/config.toml light add "localhost:26657" -c "stargate" -s "/home/ec2-user/node-stargate/data" -y --peer-id 2427F8D914A6862279B3326FA64F76E3BC06DB2E 
-cargo run --bin hermes -- -c ~/.hermes/config.toml light add "localhost:26357" -c "heliax" -s "/home/ec2-user/node-heliax/data"-y --peer-id A885BB3D3DFF6101188B462466AE926E7A6CD51E
+cargo run --bin hermes -- -c ~/.hermes/config.toml light add "localhost:26357" -c "heliax" -s "/home/ec2-user/node-heliax/data" -y --peer-id A885BB3D3DFF6101188B462466AE926E7A6CD51E
 
 # add the key seeds to the keyring of each chain
 echo "Importing keys..."
