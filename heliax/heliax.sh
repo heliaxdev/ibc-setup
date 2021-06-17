9cef3e30251634ecb5f879733ca46bcca0079cf7
Gianmarco Fraccaroli <gianmarco.fraccaroli@gmail.com>
2021-06-17 09:59:29 +0200

    [feat]: input mnemonic

--- heliax/heliax.sh
+++ heliax/heliax.sh
@@ -16,7 +16,13 @@ USER_COINS="${STAKE},${SAMOLEANS}"
 
 # add keys
 gaiad keys add validator --home=${NODE_PATH} --keyring-backend="test" --output json > ${NODE_PATH}/validator_seed.json 2> /dev/null
-gaiad keys add user --home=${NODE_PATH} --keyring-backend="test" --output json > ${NODE_PATH}/key_seed.json 2> /dev/null
+
+OUTPUT=$(gaiad --home=${NODE_PATH} keys add user --keyring-backend="test" --recover --output json)
+echo $OUTPUT > ${NODE_PATH}/key_seed.json 2> /dev/null
+read MNEMONIC
+KEY_SEED=$(jq --arg MNEMONIC "$MNEMONIC" '{ "mnemonic": $MNEMONIC } + .' ${NODE_PATH}/key_seed.json)
+echo $KEY_SEED > ${NODE_PATH}/key_seed.json
+
 gaiad add-genesis-account $(gaiad keys show validator -a --keyring-backend="test" --home=${NODE_PATH}) $STAKE --home=${NODE_PATH}
 gaiad add-genesis-account $(gaiad keys show user -a --keyring-backend="test" --home=${NODE_PATH}) $USER_COINS --home=${NODE_PATH}
 gaiad gentx validator 1000000stake --chain-id=h3liax --home=${NODE_PATH} --keyring-backend="test"
@@ -31,7 +37,7 @@ rm "${NODE_PATH}/config/app.toml"
 cp heliax/app.toml "${NODE_PATH}/config/app.toml"
 
 # gaiad tx bank send validator cosmos16j8g8ye76ccuu3nvpquk6wdqcuaejhy2amvqdh 100stake  --home=${NODE_PATH} --chain-id=h3liax --keyring-backend="test"
-# gaiad keys list --home=${NODE_PATH} --chain-id=h3liax
+# gaiad keys list --home=${NODE_PATH} --chain-id=h3liax --keyring-backend="test"
 
 # start node
 screen -d -m -S ${NAME} bash -c "gaiad start --home=${NODE_PATH} --log_level=info --x-crisis-skip-assert-invariants"
